3f22d1449c59623490661f8c64a98261c599aff8
HariAgus <haryagus97@gmail.com>
2022-04-12 14:33:40 +0700

    Create box information primary

--- app/src/main/java/com/haw/forecastapp/utils/Constants.kt
+++ app/src/main/java/com/haw/forecastapp/utils/Constants.kt
@@ -5,6 +5,7 @@ import com.haw.forecastapp.BuildConfig
 object Constants {
 
     const val BASE_URL = "https://api.openweathermap.org/"
+    const val BASE_IMAGE_URL = "https://openweathermap.org/img/wn/"
     const val API_KEY = BuildConfig.API_KEY
     const val UNITS_APY = "imperial"
 
